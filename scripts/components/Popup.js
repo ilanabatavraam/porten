ba26d62d4e803caae0396e77af398f858308d9ff
Ilana Sandler <9151o66926@gmail.com>
2023-01-04 23:42:27 +0200

    form validate

--- scripts/components/Popup.js
+++ scripts/components/Popup.js
@@ -11,8 +11,8 @@ export class Popup {
         const container = popup.querySelector('.popup__inner');
         const popupBody = popup.querySelector('.popup__body');
         const form = `<form action="https://httpbin.org/post" class="popup__form" method="post" novalidate name="${data}">
-        <input type="email" placeholder="Enter email" required name="email" class="popup__input" autocomplete="off">
-        <input type="password" placeholder="Enter password" required name="password" class="popup__input"><button class="button" type="submit" disabled>Submit</button></form>`
+        <input type="email" placeholder="Enter email" required name="email" class="popup__input" autocomplete="off"><span class="popup__error"></span>
+        <input type="password" minlength="7" placeholder="Enter password" required name="password" class="popup__input"><span class="popup__error"></span><button class="button popup__submit" type="submit" disabled>Submit</button></form>`
         container.innerHTML = form;
 
         if (data && data === 'singin') {
@@ -32,6 +32,9 @@ export class Popup {
         document.body.classList.add('body_popup-opened');
         document.addEventListener('keydown', this.keydownHandler);
         this.elem.addEventListener('click', this.closePopup);
+        this.elem.addEventListener('input', this.onChange);
+        const form = this.elem.querySelector('.popup__form');
+        form.addEventListener('submit', this.onSubmit);
     }
 
     close() {
@@ -45,11 +48,60 @@ export class Popup {
         if (event.code === 'Escape') {
           this.close();
         }
-      }
+    }
     
-      closePopup = (event) => {
+    closePopup = (event) => {
         if (event.target.closest('.popup__close') || event.target === event.currentTarget) {
-          this.close();
+            this.close();
         }
-      }
+    }
+
+    onChange = (event) => {
+        const inputsArr = Array.from(this.elem.querySelectorAll('.popup__input'));
+        const submitBtn = this.elem.querySelector('.popup__submit');
+        if (event.target.closest('.popup__input')) {
+            if (event.target.validity.valid) {
+                event.target.classList.remove('popup__input_style_error');
+                event.target.nextSibling.textContent = '';
+            } else {
+                event.target.classList.add('popup__input_style_error');
+                event.target.nextSibling.textContent = event.target.validationMessage;
+            }
+            const formValid = inputsArr.filter(input => input.validity.valid);
+    
+            if (formValid.length === inputsArr.length) {
+                submitBtn.removeAttribute('disabled', 'false');
+            } else {
+                submitBtn.removeAttribute('disabled', 'true');
+            }
+        }
+    }
+
+    checkValidity = () => {
+        const inputsArr = Array.from(this.elem.querySelectorAll('.popup__input'));
+        const formValid = inputsArr.filter(input => input.validity.valid);
+        if (formValid.length === inputsArr.length) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault();
+        if (this.checkValidity()) {
+            let formData = new FormData(event.target);
+            fetch('https://httpbin.org/post', { method: "POST", body: formData })
+            .then((response) => {
+                return response.json;
+            })
+            .then(() => {
+                const body = this.elem.querySelector('.popup__inner');
+                const popupBody = this.elem.querySelector('.popup__body');
+                body.innerHTML = '';
+                popupBody.innerHTML = 'Succes!';
+                setTimeout(() => {this.close()}, 3000);
+            });
+        }
+    }
 }
